e79427a93a652eaeb855523e3e9eb96b60349abf
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-07 12:54:47 -0300

    aula 03 variaveis

---
+++ Aula 01/variaveis_03.py
@@ -0,0 +1,12 @@
+nome = "Tony"
+idade = 28
+
+nome, idade = "Giovana", 27
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILLIAN_STATES = ["RN", "PB", "PE", "CE"]
+
+print(BRAZILLIAN_STATES)
