11b1ae32b04a47bf0e99fa37a15b7f9f7796b11b
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-07 13:31:12 -0300

    aula 04 conversoess

---
+++ conversaotipos_04.py
@@ -0,0 +1,15 @@
+# Conversoes basicas
+
+print(int(1.9745))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+
+print(type(valor))
+print(type(valor_str))
+
+print(331 / 2)
+print(331 // 2)
