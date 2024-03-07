b171668f9ad91d93f579092bf1d34b4d46c3c74a
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-07 11:42:41 -0300

    Primeiras aulas de python da DIO

---
+++ Aula 01/tiposdados_02.py
@@ -0,0 +1,10 @@
+# Tipos de dados e seus exemplos
+# Interger / Float / Boolean / String
+# Escrever python no terminal do VSCode o torna interativo
+
+print(11+10+1000)
+print(1.5+1+0.5)
+print(True)
+print(False)
+print("Python")
+
