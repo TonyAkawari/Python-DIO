4cceb9b70eabc3eb48de7c4c2eeb34abf389ee31
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-07 20:20:35 -0300

    Modulo 02 e aula 01  operadores

---
+++ Modulo 02/aula_01_operadoresari.py
@@ -0,0 +1,30 @@
+# Variáveis
+produto_1 = 20
+produto_2 = 10
+# Soma
+print(produto_1 + produto_2)
+# Subtraçao
+print(produto_1 - produto_2)
+# Divisão
+print(produto_1 / produto_2)
+# Divisão que deixa ditero em interger ao invés de float
+print(produto_1 // produto_2)
+# Multiplicação
+print(produto_1 * produto_2)
+# Módulo é o resto da divisão: O que resta após o mesmo numero caber no dividido
+print(produto_1 % produto_2)
+# Exponencial
+print(produto_1 ** produto_2)
+
+# Parenteses sempre resolve primeiro, por tanto fica 15 * 4
+x = (10 + 5) * 4
+
+# Exponencial primeiro ou seja 2**2=4 daí 25*4=100 depois o 10/2=5 pra entao 100 + 5
+y= 10 / 2 + 25 * 2 ** 2
+
+#
+z= (10/2) + 25 * ((2-2)**2)
+
+print (x)
+print (y)
+print (z)
