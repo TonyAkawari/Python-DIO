855eb4ad1b9d3df397213c7e6c66dadc82119868
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-11 14:54:33 -0300

    Modulo02_Aula05

---
+++ Modulo 02/aula_05_operadores_identidade.py
@@ -0,0 +1,13 @@
+# Operador is e o is not servem para saber se os comparados estão na mesma região de memória
+
+saldo = 1000
+limite = 1000
+
+print (saldo is limite)
+print(saldo is not limite)
+
+saldo_1 = 1000
+limite_1=500
+
+print (saldo_1 is limite_1)
+print(saldo_1 is not limite_1)
