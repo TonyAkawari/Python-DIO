ee0f810af909d645a9a6c243f8abb09b816e1d74
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-11 13:50:02 -0300

    Modulo02 aula 03

---
+++ Modulo 02/aula_03_operadores_atribuicao.py
@@ -0,0 +1,34 @@
+saldo = 500
+print(saldo)
+
+
+# Atribuiçao simples =
+saldo = 200
+print(saldo)
+
+# Atribuiçao com adição +=
+saldo += 10
+print(saldo)
+
+# Atribuiçao com subtração -=
+saldo -= 5
+print(saldo)
+
+# Atribuiçao com divisão e divisão inteira /= e //=
+saldo //= 2
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+# Atribuiçao com multiplicação *=
+saldo *= 10
+print(saldo)
+
+# Atribuiçao com módulo (resto da divisão) %=
+saldo %= 4
+print(saldo)
+
+# Atribuiçao com exponenciação **=
+saldo **= 2
+print(saldo)
