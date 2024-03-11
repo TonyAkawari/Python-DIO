12d15152ca91899d405723944f713c98ca0f973c
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-11 15:02:14 -0300

    Modulo02_Aula06

---
+++ Modulo 02/aula_06_operadores_associacao.py
@@ -0,0 +1,10 @@
+# Operador de associação: in/not in, para saber se algum conteúdo está ou não em uma variável.
+# Se estiver, True. Se não estiver False.
+
+frutas =["limão", "uva"]
+print("laranja" in frutas) #false
+
+
+frutas_1 =["limão", "uva", "laranja"]
+print("laranja" in frutas_1) #true
+print("limão" not in frutas_1) #false
