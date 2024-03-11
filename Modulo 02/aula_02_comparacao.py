4303c37689b77b1c498377bd7b07109cbe011b8c
Tony Akawari <antoniocordeirolimajunior@gmail.com>
2024-03-11 13:34:52 -0300

    Modulo 02 aula 02

---
+++ Modulo 02/aula_02_comparacao.py
@@ -0,0 +1,16 @@
+saldo = 200
+saque = 200
+
+# Vao retornar como True ou False
+# Equivalente dá True se for, Falso se não
+print(saldo == saque)
+# Diferente dá True se for, Falso se não
+print(saldo != saque)
+# Maior que dá True se for, Falso se não
+print(saldo > saque)
+# Maior ou igual dá True se for, Falso se não
+print(saldo >= saque)
+# Menor que dá True se for, Falso se não
+print(saldo < saque)
+# Menor ou igual dá True se for, Falso se não
+print(saldo <= saque)
